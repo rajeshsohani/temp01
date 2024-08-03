29ca5048a8877873340380def43658cf5f37b091
rajeshsohani <78219018+rajeshsohani@users.noreply.github.com>
2024-08-03 09:45:51 +0530

    urls.py

--- WebSiteCore01/urls.py
+++ WebSiteCore01/urls.py
@@ -27,3 +27,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
