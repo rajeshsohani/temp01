6f86358470e9c0491a3a9b8d3e7bd4f9b60b43c6
rajeshsohani <78219018+rajeshsohani@users.noreply.github.com>
2024-08-03 09:49:57 +0530

    settings.py

--- WebSiteCore01/settings.py
+++ WebSiteCore01/settings.py
@@ -126,7 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 #STATIC_URL = 'static/'
-STATIC_URL = 'Core01/static/'
+#STATIC_URL = 'Core01/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'#addes this
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 
